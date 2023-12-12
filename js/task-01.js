a9915f540145d6e1897d5319a780fb1afb451035
MimesisV <141736479+MimesisV@users.noreply.github.com>
2023-12-12 14:47:30 +0200

    homeWork

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelector("categories");
-const items = document.querySelectorAll(".item");
-
-console.log("Number of categories: ", items.length);
-
-items.forEach(item => {
-    console.log(`Categories:  ${item.firstElementChild.textContent}`);
-    console.log(`Elements:  ${item.querySelectorAll('ul li').length}`);
-})
